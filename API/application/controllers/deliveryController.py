c3910ba2431bbb4c3cce6de053f8bb7ba4445785
Romain Fanara <romain.fanara@gmail.com>
2015-12-05 14:41:38 +0000

    deliveryController +tests

--- API/application/controllers/deliveryController.py
+++ API/application/controllers/deliveryController.py
@@ -1,5 +1,6 @@
-from flask import render_template, Blueprint, url_for, redirect, flash, request
+from flask import abort, Blueprint, request, jsonify
 from application import db
+from application.classes.Location import Location
 
 
 delivery_blueprint = Blueprint('delivery', __name__,)
@@ -7,76 +8,184 @@ delivery_blueprint = Blueprint('delivery', __name__,)
 
 @delivery_blueprint.route("/deliveries", methods=['POST'])
 def create():
-    ID = ""
-    date_created = ""
-    date_pickup = ""
-    date_delivery = ""
-
-    Location_delivery = ""
-    Location_pickup = ""
-
-    customer_ID = ""
-    driver_ID = ""
-
     delivery = request.get_json(force=True)
-
     if (
-        "delivery" in delivery["delivery"] and
+        "delivery" in delivery and
         "location_delivery" in delivery["delivery"] and
         "location_pickup" in delivery["delivery"] and
-        "customer_ID" in delivery["delivery"] and
-        "driver_ID" in delivery["delivery"]and
+        "customer_id" in delivery["delivery"] and
         "date_created" in delivery["delivery"]and
         ("lat" and "lng" in delivery["delivery"]["location_delivery"]) and
         ("lat" and "lng" in delivery["delivery"]["location_pickup"])
     ):
 
         # record locations
-        loc_delivery_ID= db.select(table="locations",
-                                   selected_columns=("id",),
-                                   conditions=delivery["delivery"]["location_delivery"],
-                                   multiple=False)
+        location_delivery_id= Location.getIdFromDB(dbInstance=db,
+                                                   lat=delivery["delivery"]["location_delivery"]["lat"],
+                                                   lng=delivery["delivery"]["location_delivery"]["lng"])
 
-        loc_pickup_ID= db.select(table="locations",
-                                   selected_columns=("id",),
-                                   conditions=delivery["delivery"]["location_pickup"],
-                                   multiple=False)
+        location_pickup_id= Location.getIdFromDB(dbInstance=db,
+                                                   lat=delivery["delivery"]["location_pickup"]["lat"],
+                                                   lng=delivery["delivery"]["location_pickup"]["lng"])
 
+        # check existing customer
+        if not db.is_existing(table="customers", conditions={"id": delivery["delivery"]["customer_id"]}) :
+            return jsonify(info="Customer not found"),404
 
-        if loc_delivery_ID is None:
-            loc_delivery_ID=db.insert(table="locations", params=delivery["delivery"]["location_delivery"])
+        formatted_delivery ={}
+        formatted_delivery["location_delivery_id"] = location_delivery_id
+        formatted_delivery["location_pickup_id"] = location_pickup_id
+        formatted_delivery["customer_id"] = delivery["delivery"]["customer_id"]
+        formatted_delivery["date_created"] = delivery["delivery"]["date_created"]
 
-        if loc_pickup_ID is None:
-            loc_pickup_ID=db.insert(table="locations", params=delivery["delivery"]["location_pickup"])
+        if "date_pickup" in delivery["delivery"]:
+            formatted_delivery["date_pickup"] = delivery["delivery"]["date_pickup"]
 
-        # record delivery
+        if "date_delivery" in delivery["delivery"]:
+            formatted_delivery["date_delivery"] = delivery["delivery"]["date_delivery"]
 
-        # db.insert(table="deliveries", params )
+        delivery_id = db.insert(table="deliveries", params=formatted_delivery )
 
-        data={
-            ""
-        }
+        return jsonify(info="Delivery created successfully", deliveryId=delivery_id),200
 
     else:
-        pass
+        abort(400)
 
 
 
 @delivery_blueprint.route("/deliveries/<id>", methods=['PUT'])
 def update(id:int):
-    pass
+    if not db.is_existing(table="deliveries", conditions={"id":id}):
+        return jsonify(info="Delivery not found"), 404
+
+    delivery = request.get_json(force=True)
+
+    if "delivery" in delivery:
+        formatted_delivery ={}
+
+        #customer_id
+        if "customer_id" in delivery["delivery"]:
+            if not db.is_existing(table="customers", conditions={"id": delivery["delivery"]["customer_id"]}):
+                return jsonify(info="Customer not found"), 404
+            formatted_delivery["customer_id"] = delivery["delivery"]["customer_id"]
+
+        #location pickup
+        if "location_pickup" in delivery["delivery"]:
+            if "lat" in delivery["delivery"]["location_pickup"] and "lng" in delivery["delivery"]["location_pickup"]:
+                formatted_delivery["location_pickup_id"]= Location.getIdFromDB(dbInstance=db,
+                                                         lat=delivery["delivery"]["location_pickup"]["lat"],
+                                                         lng=delivery["delivery"]["location_pickup"]["lng"])
+        #location delivery
+        if "location_delivery" in delivery["delivery"]:
+            if "lat" in delivery["delivery"]["location_delivery"] and "lng" in delivery["delivery"]["location_delivery"]:
+                formatted_delivery["location_delivery_id"] = Location.getIdFromDB(dbInstance=db,
+                                                             lat=delivery["delivery"]["location_delivery"]["lat"],
+                                                             lng=delivery["delivery"]["location_delivery"]["lng"])
+        #date
+        if "date_pickup" in delivery["delivery"]:
+            formatted_delivery["date_pickup"] = delivery["delivery"]["date_pickup"]
+
+        if "date_delivery" in delivery["delivery"]:
+            formatted_delivery["date_delivery"] = delivery["delivery"]["date_delivery"]
+
+        db.update(table="deliveries", params=formatted_delivery, conditions={"id": id})
+        return jsonify(info="Delivery updated successfully"), 200
+    else :
+        abort(400)
+
 
 
 @delivery_blueprint.route("/deliveries/<id>", methods=['DELETE'])
 def delete(id:int):
-    pass
+    if not db.is_existing(table="deliveries", conditions={"id":id}):
+        return jsonify(info="Delivery not found"), 404
+
+    db.delete(table="deliveries", conditions={"id":id})
+    return jsonify(info="Delivery deleted successfully"),200
+
 
 
 @delivery_blueprint.route("/deliveries/<id>", methods=['GET'])
 def get(id:int):
-    pass
+
+    _SQL = """SELECT deliveries.*,
+            pickup.lat AS location_pickup_lat ,
+            pickup.lng AS location_pickup_lng,
+            delivery.lat AS location_delivery_lat ,
+            delivery.lng AS location_delivery_lng
+
+            FROM deliveries
+            INNER JOIN locations AS pickup ON deliveries.location_pickup_id = pickup.id
+            INNER JOIN locations AS delivery ON deliveries.location_delivery_id = delivery.id
+            WHERE deliveries.id = %(id)s;
+          """
+
+    delivery=db.query(_SQL, {"id": id}, multiple=False)
+
+    if delivery is None:
+        return jsonify(info="Delivery not found"), 404
+
+    d = {
+        "id": delivery["id"],
+        "customer_id": delivery["customer_id"],
+        "driver_id": delivery["driver_id"],
+        "date_pickup": delivery["date_pickup"],
+        "date_delivery": delivery["date_delivery"],
+        "date_created": delivery["date_created"],
+        "location_pickup": {
+            "lat": delivery["location_pickup_lat"],
+            "lng": delivery["location_pickup_lng"],
+        },
+        "location_delivery": {
+            "lat": delivery["location_delivery_lat"],
+            "lng": delivery["location_delivery_lng"],
+        }
+    }
+    return jsonify(delivery=d), 200
+
 
 
 @delivery_blueprint.route("/deliveries/all", methods=['GET'])
 def getAll():
-    pass
+
+    _SQL = """ SELECT deliveries.*,
+            pickup.lat AS location_pickup_lat ,
+            pickup.lng AS location_pickup_lng,
+            delivery.lat AS location_delivery_lat ,
+            delivery.lng AS location_delivery_lng
+
+            FROM deliveries
+            INNER JOIN locations AS pickup ON deliveries.location_pickup_id = pickup.id
+            INNER JOIN locations AS delivery ON deliveries.location_delivery_id = delivery.id;
+          """
+
+    deliveries_raw = db.query(_SQL)
+
+    if len(deliveries_raw) <1:
+        return jsonify(deliveries=deliveries_raw), 200
+
+    deliveries = []
+
+    for delivery in deliveries_raw:
+        d = {
+            "delivery" : {
+                "id" : delivery["id"],
+                "customer_id" : delivery["customer_id"],
+                "driver_id" : delivery["driver_id"],
+                "date_pickup" : delivery["date_pickup"],
+                "date_delivery" : delivery["date_delivery"],
+                "date_created" : delivery["date_created"],
+                "location_pickup" : {
+                    "lat" : delivery["location_pickup_lat"],
+                    "lng" : delivery["location_pickup_lng"],
+                },
+                "location_delivery" : {
+                    "lat" : delivery["location_delivery_lat"],
+                    "lng" : delivery["location_delivery_lng"],
+                }
+            }
+        }
+        deliveries.append(d)
+
+    return jsonify(deliveries=deliveries), 200
+
---
+++ API/application/test/deliveryTest.py
@@ -0,0 +1,240 @@
+
+from flask import url_for
+import pytest_mock
+#from unittest.mock import patch
+import pytest
+from application.core.DBHandler import DBHandler
+from application.classes.Location import Location
+import json
+import datetime
+
+
+class TestBlueprintDelivery:
+
+
+    delivery = {
+        "delivery":{
+            "location_delivery": {
+                "lat" : 12.4354645,
+                "lng" : 34.3453453
+            },
+            "location_pickup" : {
+                "lat" : 17.435645,
+                "lng" : 35.3453453
+            },
+            "customer_id" : 12,
+            "date_created" : '2015-11-30 17:23:08.321167',
+            "date_pickup" : '2015-11-30 17:23:09.321167',
+            "date_delivery" : '2015-11-30 17:23:10.321167'
+        }
+    }
+
+
+############### CREATE #####################
+    
+    def test_create_missing_params(self, client):
+        wrong_delivery = {}
+        res = client.post(url_for("delivery.create"), data=json.dumps(wrong_delivery), content_type='application/json')
+        assert res.status_code == 400
+        assert res.json == {'info': 'Bad request, some required fields are not recognized'}
+
+
+    def test_create_with_wrong_customer_id(self, client, mocker):
+        mocker.patch.object(DBHandler, "select", return_value={"id": 23})
+        mocker.patch.object(DBHandler, "is_existing", return_value=False)
+        res = client.post(url_for("delivery.create"), data=json.dumps(self.delivery), content_type='application/json')
+        assert res.status_code == 404
+        res.json = {"info": "Customer not found"}
+        mocker.stopall()
+
+
+    def test_create_success(self, client, mocker):
+        mocker.patch.object(DBHandler, "select", return_value={"id": 23})
+        mocker.patch.object(DBHandler, "is_existing", return_value=True)
+        mocker.patch.object(DBHandler, "insert", return_value={"id":34})
+        res = client.post(url_for("delivery.create"), data=json.dumps(self.delivery), content_type='application/json')
+        assert res.status_code == 200
+        res.json = {"info": "Delivery created successfully", "deliveryId": 34}
+        mocker.stopall()
+
+
+
+############### UPDATE #####################
+
+    def test_update_with_wrong_delivery_id(self, client, mocker):
+        mocker.patch.object(DBHandler, "is_existing", return_value=False)
+        res = client.put(url_for("delivery.update", id=12))
+        assert res.status_code == 404
+        assert res.json == {"info" : "Delivery not found"}
+        mocker.stopall()
+
+
+    def test_update_with_wrong_params(self, client, mocker):
+        mocker.patch.object(DBHandler, "is_existing", return_value=True)
+        res = client.put(url_for("delivery.update", id=12),  data=json.dumps({}), content_type='application/json')
+        assert res.status_code == 400
+        assert res.json == {'info': 'Bad request, some required fields are not recognized'}
+        mocker.stopall()
+    
+
+    def test_update_with_wrong_customer(self, client, mocker):
+        def is_existing(table, **kwargs):
+            return table == "deliveries"
+        
+        wrong_delivery = {
+            "delivery":{
+                "customer_id" : 2
+            }
+        }    
+        mocker.patch.object(DBHandler, "is_existing", side_effect=is_existing)
+        res = client.put(url_for("delivery.update", id=12),  data=json.dumps(wrong_delivery), content_type='application/json')
+        assert res.status_code == 404
+        assert res.json == {"info" : "Customer not found"}
+        mocker.stopall()
+    
+
+    def test_update_with_success(self, client, mocker):
+        _delivery = {
+            "delivery":{
+                "customer_id" : 2,
+                "location_delivery" : {
+                    "lat": 23.4555,
+                    "lng": 45.5677
+                },
+                "location_pickup" : {
+                    "lat": 22.4555,
+                    "lng": 4.5677
+                },
+                "date_pickup" : "13/12/2015",
+                "date_delivery" : "13/01/2016"
+            }
+        }
+        mocker.patch.object(Location, "getIdFromDB", return_value=34)
+        mocker.patch.object(DBHandler, "is_existing", return_value=True)
+        mocker.patch.object(DBHandler, "update", return_value=True)
+        res = client.put(url_for("delivery.update", id=12),  data=json.dumps(_delivery), content_type='application/json')
+        assert res.status_code == 200
+        assert res.json == {"info" : "Delivery updated successfully"}
+        mocker.stopall()
+
+
+
+############### DELETE #####################
+
+
+    def test_delete_with_wrong_delivery_id(self, client, mocker):
+        mocker.patch.object(DBHandler, "is_existing", return_value=False)
+        res = client.delete(url_for("delivery.delete", id=12))
+        assert res.status_code == 404
+        assert res.json == {"info" : "Delivery not found"}
+
+
+
+    def test_delete_with_success(self, client, mocker):
+        mocker.patch.object(DBHandler, "is_existing", return_value=True)
+        mocker.patch.object(DBHandler, "delete", return_value=True)
+        res = client.delete(url_for("delivery.delete", id=12))
+        assert res.status_code == 200
+        assert res.json == {"info" : "Delivery deleted successfully"}
+
+
+############ GET #######################
+
+    def test_get_with_wrong_id(self, client, mocker):
+        mocker.patch.object(DBHandler, "select", return_value=None)
+        res = client.get(url_for("delivery.get", id=12))
+        assert res.status_code == 404
+        assert res.json == {"info" : "Delivery not found"}
+
+
+    def test_get_with_success(self, client, mocker):
+        delivery_sql = {
+            "id" : 2,
+            "customer_id" : 34,
+            "driver_id" : 5,
+            "date_pickup" : None,
+            "date_delivery" : None,
+            "date_created" : "Monday",
+            "location_pickup_lat" : 12.45654,
+            "location_pickup_lng" : 2.46654,
+            "location_delivery_lat" : 24.45654,
+            "location_delivery_lng" : 3.46654
+        }
+
+        expected_delivery = {
+            "id" : 2,
+            "customer_id" : 34,
+            "driver_id" : 5,
+            "date_pickup" : None,
+            "date_delivery" : None,
+            "date_created" : "Monday",
+             "location_pickup" : {
+                    "lat" : 12.45654,
+                    "lng" : 2.46654,
+                },
+                "location_delivery" : {
+                    "lat" : 24.45654,
+                    "lng" : 3.46654
+                }
+        }
+        mocker.patch.object(DBHandler, "query", return_value=delivery_sql)
+        res = client.get(url_for("delivery.get", id=2))
+        assert res.status_code == 200
+        assert "delivery" in res.json
+
+        assert res.json["delivery"] == expected_delivery
+
+
+############## GET ALL ########################
+
+
+    def test_get_all_with_no_recorded_delivery(self, client, mocker):
+        mocker.patch.object(DBHandler, "query", return_value=[])
+        res = client.get(url_for("delivery.getAll"))
+        assert res.status_code == 200
+        assert "deliveries" in res.json
+        assert isinstance(res.json["deliveries"], list)
+        assert len(res.json["deliveries"]) == 0
+
+
+
+    def test_get_all_with_success(self, client, mocker):
+        delivery_sql = [{
+            "id" : 2,
+            "customer_id" : 34,
+            "driver_id" : 5,
+            "date_pickup" : None,
+            "date_delivery" : None,
+            "date_created" : "Monday",
+            "location_pickup_lat" : 12.45654,
+            "location_pickup_lng" : 2.46654,
+            "location_delivery_lat" : 24.45654,
+            "location_delivery_lng" : 3.46654
+        }]
+
+        expected_delivery = {
+            "delivery" : {
+                "id" : 2,
+                "customer_id" : 34,
+                "driver_id" : 5,
+                "date_pickup" : None,
+                "date_delivery" : None,
+                "date_created" : "Monday",
+                 "location_pickup" : {
+                        "lat" : 12.45654,
+                        "lng" : 2.46654,
+                    },
+                    "location_delivery" : {
+                        "lat" : 24.45654,
+                        "lng" : 3.46654
+                    }
+
+                }
+        }
+        mocker.patch.object(DBHandler, "query", return_value=delivery_sql)
+        res = client.get(url_for("delivery.getAll"))
+        assert res.status_code == 200
+        assert "deliveries" in res.json
+        assert isinstance(res.json["deliveries"], list)
+        assert res.json["deliveries"][0] == expected_delivery
+
